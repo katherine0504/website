44923798616389eefc26c483ecc5eea4d64638cd
katherine0504 <F74036069@nkcu.edu.tw>
2017-08-31 17:20:02 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// Set static path
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(3000, function() {
+  console.log('Server started on Port 3000 .... ');
+})
